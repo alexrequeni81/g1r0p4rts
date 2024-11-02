3b52850bdaf1f51bc248842916c2b8d1569afd7e
alexrequeni81 <35029740+alexrequeni81@users.noreply.github.com>
2024-11-02 12:46:49 +0100

    Update authMiddleware.js

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).send('Acceso denegado.');
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).send('Token no válido.');
+  }
+};
+
+module.exports = authMiddleware;
