344257c7ec97f16c3dd3d756ec9728ecc0450a23
alexrequeni81 <35029740+alexrequeni81@users.noreply.github.com>
2024-11-02 12:57:38 +0100

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController');
 
 // Ejemplo de una ruta para login
 router.post('/login', authController.login);
+router.get('/users', authController.getUsers); // Nueva ruta para obtener usuarios
 
 module.exports = router; // Asegúrate de que esté exportando `router`
