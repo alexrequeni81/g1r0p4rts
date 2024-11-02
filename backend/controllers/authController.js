b38394e8f9ad4ebcb2b846fa5b66643d1a738ed9
alexrequeni81 <35029740+alexrequeni81@users.noreply.github.com>
2024-11-02 12:21:16 +0100

    Update authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,7 @@
+// backend/controllers/authController.js
+const login = (req, res) => {
+  // Lógica de autenticación
+  res.send("Login exitoso");
+};
+
+module.exports = { login }; // Exporta la función como un objeto
