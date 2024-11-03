ba280824cccbeb625112e4827c5bec02a71a43be
alexrequeni81 <35029740+alexrequeni81@users.noreply.github.com>
2024-11-03 13:40:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 async function fetchData() {
     try {
-        const usersResponse = await fetch('http://localhost:3000/api/users');
+        const usersResponse = await fetch('https://g1r0p4rts.onrender.com/api/users');
         const users = await usersResponse.json();
         const usersDiv = document.getElementById('users');
         usersDiv.innerHTML = JSON.stringify(users, null, 2);
 
-        const partsResponse = await fetch('http://localhost:3000/api/parts');
+        const partsResponse = await fetch('https://g1r0p4rts.onrender.com/api/parts');
         const parts = await partsResponse.json();
         const partsDiv = document.getElementById('parts');
         partsDiv.innerHTML = JSON.stringify(parts, null, 2);
