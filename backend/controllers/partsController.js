cc625eca8557470992dbe873ea11ec826251ec28
alexrequeni81 <35029740+alexrequeni81@users.noreply.github.com>
2024-11-03 10:30:07 +0100

    Delete backend directory

--- backend/controllers/partsController.js
+++
@@ -1,7 +0,0 @@
-// backend/controllers/partsController.js
-const getParts = (req, res) => {
-  // Lógica para obtener los repuestos
-  res.send("Lista de repuestos");
-};
-
-module.exports = { getParts }; // Exporta la función como un objeto
