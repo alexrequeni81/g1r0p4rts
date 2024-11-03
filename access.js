3b27ca49fb97b3fa1dee205caf43e259c7930d9b
alexrequeni81 <35029740+alexrequeni81@users.noreply.github.com>
2024-11-03 18:08:04 +0100

    Create access.js

---
+++ access.js
@@ -0,0 +1,23 @@
+document.getElementById('accessForm').addEventListener('submit', async (event) => {
+    event.preventDefault(); // Evitar el envío del formulario por defecto
+
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+
+    try {
+        const response = await fetch(`https://g1r0p4rts.onrender.com/api/users?nombre=${nombre}&email=${email}`);
+        if (response.ok) {
+            const user = await response.json();
+            if (user.length > 0) {
+                // Usuario encontrado, redirigir al formulario de compra
+                window.location.href = 'purchase.html';
+            } else {
+                alert('Usuario no encontrado. Por favor, verifica los datos.');
+            }
+        } else {
+            alert('Error al verificar el usuario.');
+        }
+    } catch (error) {
+        console.error('Error al enviar los datos:', error);
+    }
+});
